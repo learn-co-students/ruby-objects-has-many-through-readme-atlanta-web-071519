bafd81367519cef466d310b37f6ef22748a15441
Raza <rtshareef@gmail.com>
2019-07-27 20:29:07 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class Customer
-  
+    attr_reader :name,:age
+    @@all = []
+    def initialize(name,age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+     
+
+    def new_meal(waiter,total,tip)
+        Meal.new(waiter,self,total,tip)
+    end
+
+    def meals
+        Meal.all.select{|m| m.customer == self}
+        
+    end
+
+    def waiters 
+        meals.map do |meal|
+            meal.waiter
+
+           
+        
+        
+        # Meal.all.map do |m|
+        #  m.waiter
+        #binding.pry
+       
+    end 
+        end
+    
+
 end
